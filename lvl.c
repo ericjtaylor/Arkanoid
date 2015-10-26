8a32d0ed00b3e0e1fd1703e95ffedd2a7e5a7dac
Mohamad Issawi <mo@openmailbox.org>
2015-10-26 01:45:58 +0000

    added lvlgen skeleton

--- lvl.c
+++ lvl.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
+#define WELL_WIDTH 7 // width is given in bricks
+
 int main()
 {
   FILE *f;
-  char buf[8];
+  char buf[256];
+  int c;
 
   f = fopen("stage1.lvl", "r");
   while (fgets (buf, sizeof(buf), f))
     {
-      printf("%s", buf);
+      c = 0;
+      while (buf[c] != '\n' && c < WELL_WIDTH * 2)
+	{
+	  
+	  if (buf[c] == '1')
+	    {
+	      printf("yes ");
+	    }
+	  else if (buf[c] == '0')
+	    {
+	      printf("no ");
+	    } 
+	  c++; 
+	}
+      printf("\n");
     }
   fclose(f);
+  return 0;
 }
