552b14e9d006eaf08ecd05650d4cd134fed3448b
Mohamad Issawi <mo@openmailbox.org>
2015-11-07 15:46:15 -0500

    improved the construction of the array

--- lvl.c
+++ lvl.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 #define WELL_WIDTH 11
-#define WELL_HEIGHT 13
+#define WELL_HEIGHT 14
 
 void make_lvl(char *stage, int brix[][WELL_HEIGHT])
 {
@@ -11,27 +11,19 @@ void make_lvl(char *stage, int brix[][WELL_HEIGHT])
   char buf[256];
   int x;
   int y = 0;
+  int c;
 
   f = fopen(stage, "r");
   while (fgets (buf, sizeof(buf), f))
     {
       x = 0;
-      y++;
       while (buf[x] != '\n' && x < WELL_WIDTH)
 	{
-	  
-	  if (buf[x] == '1')
-	    {
-	      brix[x][y] = 1;
-	      //printf("%d,%d\t", x, y);
-	    }
-	  else if (buf[x] == '0')
-	    {
-	      brix[x][y] = 0;
-	    }
+	  c = buf[x] - '0'; // cast char to int
+	  brix[x][y] = c;
 	  x++;
 	}
-      //printf("\n");
+      y++;
     }
   fclose(f);
 }
@@ -40,6 +32,22 @@ int main()
 {
   int brix[ WELL_WIDTH ][ WELL_HEIGHT ];
   make_lvl("stage1.lvl", brix); //example call
-  printf("%d", brix[0][0]);     //use case
+
+  // check to make sure that the values are correct
+  /* 
+  int x =0;
+  int y =0;
+  while (y < 14)
+    {
+      while (x < 11)
+	{
+	  printf("%d", brix[x][y]);     //use case
+	  x++;
+	}
+      x = 0;
+      printf("\n");
+      y ++;
+    }
+  */
   return 0;
 }
